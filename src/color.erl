daf6be1d6411afe469426865211dc5b4410d94d2
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-04-21 14:19:25 +0200

    add convenience {color_name}/2 functions

--- src/color.erl
+++ src/color.erl
@@ -4,13 +4,28 @@
 -export([p/2]).
 
 -export([black/1]).
+-export([black/2]).
+
 -export([red/1]).
+-export([red/2]).
+
 -export([green/1]).
+-export([green/2]).
+
 -export([yellow/1]).
+-export([yellow/2]).
+
 -export([blue/1]).
+-export([blue/2]).
+
 -export([purple/1]).
+-export([purple/2]).
+
 -export([cyan/1]).
+-export([cyan/2]).
+
 -export([white/1]).
+-export([white/2]).
 
 -define(reset, <<"\e[0m">>).
 
@@ -34,27 +49,66 @@ p(IOData, Opts) ->
 % @doc Color the text black.
 -spec black(iodata()) -> iodata().
 black(IOData)  -> p(IOData, [black]).
+
+% @doc Color the format-text black.
+-spec black(string(), [term()]) -> iodata().
+black(FormatString, FormatArgs)  -> black(io_lib:format(FormatString, FormatArgs)).
+
 % @doc Color the text red.
 -spec red(iodata()) -> iodata().
 red(IOData)    -> p(IOData, [red]).
+
+% @doc Color the format-text red.
+-spec red(string(), [term()]) -> iodata().
+red(FormatString, FormatArgs)  -> red(io_lib:format(FormatString, FormatArgs)).
+
 % @doc Color the text green.
 -spec green(iodata()) -> iodata().
 green(IOData)  -> p(IOData, [green]).
+
+% @doc Color the format-text green.
+-spec green(string(), [term()]) -> iodata().
+green(FormatString, FormatArgs)  -> green(io_lib:format(FormatString, FormatArgs)).
+
 % @doc Color the text yellow.
 -spec yellow(iodata()) -> iodata().
 yellow(IOData) -> p(IOData, [yellow]).
+
+% @doc Color the format-text yellow.
+-spec yellow(string(), [term()]) -> iodata().
+yellow(FormatString, FormatArgs)  -> yellow(io_lib:format(FormatString, FormatArgs)).
+
 % @doc Color the text blue.
 -spec blue(iodata()) -> iodata().
 blue(IOData)   -> p(IOData, [blue]).
+
+% @doc Color the format-text blue.
+-spec blue(string(), [term()]) -> iodata().
+blue(FormatString, FormatArgs)  -> blue(io_lib:format(FormatString, FormatArgs)).
+
 % @doc Color the text purple.
 -spec purple(iodata()) -> iodata().
 purple(IOData) -> p(IOData, [purple]).
+
+% @doc Color the format-text purple.
+-spec purple(string(), [term()]) -> iodata().
+purple(FormatString, FormatArgs)  -> purple(io_lib:format(FormatString, FormatArgs)).
+
 % @doc Color the text cyan.
 -spec cyan(iodata()) -> iodata().
 cyan(IOData)   -> p(IOData, [cyan]).
+
+% @doc Color the format-text cyan.
+-spec cyan(string(), [term()]) -> iodata().
+cyan(FormatString, FormatArgs)  -> cyan(io_lib:format(FormatString, FormatArgs)).
+
 % @doc Color the text white.
 -spec white(iodata()) -> iodata().
 white(IOData)  -> p(IOData, [white]).
+
+% @doc Color the format-text white.
+-spec white(string(), [term()]) -> iodata().
+white(FormatString, FormatArgs)  -> white(io_lib:format(FormatString, FormatArgs)).
 
 %--- Internal Functions -------------------------------------------------------
 
